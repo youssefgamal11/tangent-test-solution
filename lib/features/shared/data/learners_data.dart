e4936270e3ed9b8ef0d51f36c9206716199f7160
youssef gamal <youssefgamal734@gmail.com>
2026-06-29 21:03:12 +0300

    feat : change the app icon and title for android and ios

--- lib/features/shared/data/learners_data.dart
+++ lib/features/shared/data/learners_data.dart
@@ -2,26 +2,32 @@ import 'package:tangent_test_solution/features/shared/models/learner_model.dart'
 
 const mockLearners = [
   LearnerModel(
+    name: 'Youssef G.',
+    phone: '+20 1055564756',
+    level: 'Intermediate',
+    wordsLearned: 312,
+  ),
+  LearnerModel(
     name: 'Sara K.',
-    phone: '+20 101 *** ****',
+    phone: '+20 1055564756',
     level: 'Intermediate',
     wordsLearned: 342,
   ),
   LearnerModel(
     name: 'Mohamed R.',
-    phone: '+20 112 *** ****',
+    phone: '+20 1055564756',
     level: 'Intermediate',
     wordsLearned: 289,
   ),
   LearnerModel(
     name: 'Nour A.',
-    phone: '+20 115 *** ****',
+    phone: '+20 1055564756',
     level: 'Intermediate',
     wordsLearned: 401,
   ),
   LearnerModel(
     name: 'Karim T.',
-    phone: '+20 106 *** ****',
+    phone: '+20 1055564756',
     level: 'Intermediate',
     wordsLearned: 217,
   ),
